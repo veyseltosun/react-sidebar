39cc56c3b57a61b89547c9b7d61d17e8e4241784
veyseltosun <postalarim1923@gmail.com>
2022-11-13 01:48:52 +0300

    building up components but not functionality.

--- sidebar/src/Sidebar.jsx
+++ sidebar/src/Sidebar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import logo from "./logo.svg";
+import {FaTimes} from "react-icons/fa";
+import {social, links} from "./data";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <aside className={`sidebar show-sidebar`}>
+        <div className="sidebar-header">
+            <img src={logo} className="logo" alt='codding addict'/>
+            <button className='close-btn' >
+                <FaTimes/>
+            </button> 
+        </div>
+
+    </aside>
   )
 }
 
